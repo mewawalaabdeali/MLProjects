988d122c4a04757bcc97c23b6c5e8d1f9f1f2201
ali <ali_abde37@outlook.com>
2024-09-14 16:02:28 -0400

    Data Transformation also completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,10 @@ from src.exception import CustomException
 from src.logger import logging
 
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', "train.csv")
@@ -52,4 +56,8 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_tranformation(train_data,test_data)
